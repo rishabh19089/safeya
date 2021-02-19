099e546a92d705fd287ea1f5b8f42302bdd536c3
rishabh19089 <57834525+rishabh19089@users.noreply.github.com>
2021-02-20 03:32:02 +0530

    Add files via upload

---
+++ Data Files/assignments.sql
@@ -0,0 +1,12 @@
+USE b1hjbod5x53zcqbm37to;
+
+CREATE TABLE `assignments` (
+  `ID` int NOT NULL,
+  `Complaint_ID` int NOT NULL PRIMARY KEY
+);
+
+INSERT INTO `assignments` (`ID`,`Complaint_ID`) VALUES (120,1),(125,2),(91,3),(136,4),(122,5),(134,6),(70,7),(74,8),(55,9),(78,10);
+INSERT INTO `assignments` (`ID`,`Complaint_ID`) VALUES (125,11),(54,12),(61,13),(73,14),(57,15),(140,16),(117,17),(131,18),(53,19),(123,20);
+INSERT INTO `assignments` (`ID`,`Complaint_ID`) VALUES (71,21),(65,22),(135,23),(92,24),(59,25),(132,26),(76,27),(54,28),(82,29),(90,30);
+INSERT INTO `assignments` (`ID`,`Complaint_ID`) VALUES (54,81),(73,82),(130,83),(67,84),(131,85),(118,86),(125,87),(51,88),(96,89),(58,90);
+INSERT INTO `assignments` (`ID`,`Complaint_ID`) VALUES (70,91),(75,92),(136,93),(133,94),(105,95),(114,96),(137,97),(76,98),(147,99),(138,100);
---
+++ Data Files/assigns.sql
@@ -0,0 +1,17 @@
+USE b1hjbod5x53zcqbm37to;
+
+CREATE TABLE `assigns` (
+  `Worker_ID` int NOT NULL,
+  `FMS_ID` int NOT NULL,
+  `Complaint_ID` int primary key
+);
+
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (142,9,1),(105,1,2),(142,10,3),(107,10,4),(55,9,5),(146,1,6),(77,10,7),(93,4,8),(94,10,9),(51,4,10);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (60,4,11),(92,1,12),(52,10,13),(68,3,14),(109,4,15),(80,10,16),(147,8,17),(128,1,18),(148,2,19),(56,10,20);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (64,4,21),(72,8,22),(96,6,23),(79,1,24),(65,5,25),(119,8,26),(131,2,27),(86,3,28),(72,10,29),(96,9,30);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (56,8,31),(142,6,32),(105,8,33),(135,7,34),(71,7,35),(69,7,36),(90,1,37),(70,4,38),(137,5,39),(54,10,40);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (103,2,41),(104,8,42),(118,1,43),(100,7,44),(126,9,45),(124,10,46),(140,9,47),(127,4,48),(117,3,49),(114,1,50);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (96,1,51),(145,6,52),(69,5,53),(51,1,54),(130,9,55),(104,6,56),(75,8,57),(132,4,58),(56,7,59),(108,1,60);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (74,7,61),(94,2,62),(63,7,63),(100,4,64),(61,3,65),(124,10,66),(140,10,67),(128,2,68),(149,4,69),(77,6,70);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (71,7,81),(84,5,82),(51,8,83),(86,9,84),(135,2,85),(144,3,86),(136,9,87),(64,10,88),(115,1,89),(75,3,90);
+INSERT INTO `assigns` (`Worker_ID`,`FMS_ID`,`Complaint_ID`) VALUES (99,9,91),(76,2,92),(107,5,93),(111,4,94),(91,7,95),(120,1,96),(89,7,97),(108,10,98),(102,10,99),(85,4,100);
