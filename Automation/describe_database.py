3b0384e7936af913e75dde2b0db7a01cc2f457e6
rishabh19089 <57834525+rishabh19089@users.noreply.github.com>
2021-02-24 15:09:56 +0530

    Update describe_database.py

--- Automation/describe_database.py
+++ Automation/describe_database.py
@@ -7,11 +7,21 @@ mydb = mysql.connector.connect(
     database = "b1hjbod5x53zcqbm37to")
 
 cursor = mydb.cursor()
-# cursor.execute("drop table washroom")
-# cursor.execute("SELECT * FROM washroom")
-# tables = cursor.fetchall()
 
-# print (tables)
+
+def positive_int_input(qn):
+    '''
+    Take integer input > 0.
+    Paramters: qn -> The question asked in string form.
+    Returns: The integer input.
+    '''
+    ans = -1
+    while ans<0:
+        try:
+            ans = int(input(qn))
+        except:
+            print ("Please enter an integer.")
+    return ans
 
 cols = ["FIELD", "TYPE", "NULL", "KEY", "DEFAULT", "EXTRA"]
 
@@ -19,21 +29,50 @@ cursor.execute("SHOW TABLES")
 l = cursor.fetchall()
 q = []
 for i in l:
-    if type(i[0])==str:
+    if type(i)==str:
         q.append(i[0])
     else:
         q.append(i[0].decode())
-# # mydb.commit()
-p=[]
-for i in q:
-    cursor.execute("DESC {}".format(i))
-    p.append(cursor.fetchall())
 
-print()
-for i in range(len(p)):
-    print (f"DESCRIPTION OF {q[i]}")
+print("Table Description")
+print("Showing all the options....")
+print ("\n0. All Tables")
+for i in range(1, len(q)+1):
+    print(f"{i}. {q[i-1]}")
+
+ans = positive_int_input("\nPlease enter the table number that you want described: ")
+while (ans>len(q)):
+    print("Please enter a number from 0 to {}".format(len(q)))
+    ans = positive_int_input("Please enter the table number that you want described: ")
+
+if ans==0:
+    p=[]
+    for i in q:
+        cursor.execute(f"DESC {i}")
+        p.append(cursor.fetchall())
     print()
-    for j in p[i]:
+    for i in range(len(p)):
+        print (f"DESCRIPTION OF {q[i]}")
+        print()
+        for j in p[i]:
+            print (f"Attribute - {j[0]}")
+            for t in range(1, len(j)):
+                if type(j[t])==bytearray:
+                    print (f"\t    {cols[t]}: {j[t].decode()}")
+                elif type(j[t])==bytes:
+                    print (f"\t    {cols[t]}: {j[t].decode()}")
+                else:
+                    print (f"\t    {cols[t]}: {j[t]}")
+            print()
+        print("------------------------------------------------------------\n")
+
+else:
+    i = ans-1
+    cursor.execute(f"DESC {q[i]}")
+    l = cursor.fetchall()
+    print (f"\nDESCRIPTION OF {q[i]}")
+    print()
+    for j in l:
         print (f"Attribute - {j[0]}")
         for t in range(1, len(j)):
             if type(j[t])==bytearray:
@@ -44,3 +83,7 @@ for i in range(len(p)):
                 print (f"\t    {cols[t]}: {j[t]}")
         print()
     print("------------------------------------------------------------\n")
+
+
+
+
