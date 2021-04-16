4a9bd994aadc7f42438420c5d6423dcc72ab659e
Tanmay Rajore <55682285+tanmay-rajore@users.noreply.github.com>
2021-04-16 20:53:36 +0530

    view generation

---
+++ database/view_generation.sql
@@ -0,0 +1,15 @@
+use bn2buig34duyzy4zkxh8;
+
+create view student3 as
+select Remarks,Rating,Complaint_ID from complaints;
+
+create view FMS_admin_update_complaint as
+select End_time,Resolved,Complaint_ID from complaints;
+
+create view worker1 as
+select ID,First_name, Last_name,Age,Gender,Contact_number,Duty,Temperature from workers;
+
+CREATE view complaint_assign as SELECT assigns.Complaint_ID, assigns.FMS_ID, assigns.Worker_ID, complaints.Rating, complaints.Remarks FROM assigns, complaints WHERE complaints.Complaint_ID = assigns.Complaint_ID;
+
+create view currently_working as
+select * from workers NATURAL JOIN assignments;
